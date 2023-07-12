180b2fb845ab7c28ef31d06f1eb31d378b65ddb1
Kato Taisei <katotaisei064@gmail.com>
2023-07-12 09:18:35 +0900

    define startShadowing #start_button calls startShadowing show an alert box when a YouTube player is not ready

--- script.js
+++ script.js
@@ -15,15 +15,27 @@ function onPlayerReady(){
 
 function playYouTubeVideo(){
     const input = document.getElementById("url_input")
-    if (input.reportValidity() && is_player_ready){
-        document.getElementById("start").style.display = "none"
-        document.getElementById("stage").style.display = "block"
-        player.setSize($(document).width(),
-                       $(document).height() - $("#stop_button").outerHeight(true) - lineHeight(document.getElementsByClassName("dictation")[0]) * 2)
-        const video_url = input.value
-        const video_id = video_url.substring(video_url.length - 11)
-        player.loadVideoById(video_id)
+    if (!input.reportValidity()){
+        return false
+    }
+    if (!is_player_ready){
+        alert("A YouTube player is not ready.")
+        return false
+    }
+    document.getElementById("start").style.display = "none"
+    document.getElementById("stage").style.display = "block"
+    player.setSize($(document).width(),
+                   $(document).height() - $("#stop_button").outerHeight(true) - lineHeight(document.getElementsByClassName("dictation")[0]) * 2)
+    const video_url = input.value
+    const video_id = video_url.substring(video_url.length - 11)
+    player.loadVideoById(video_id)
+    return true
+}
+
+function startShadowing(){
+    if (playYouTubeVideo()){
+
     }
 }
 
-document.getElementById("start_button").addEventListener("click", playYouTubeVideo)
+document.getElementById("start_button").addEventListener("click", startShadowing)
