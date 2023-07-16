042e0f6e58af6044f591d8f28773962c6bf13604
Kato Taisei <katotaisei064@gmail.com>
2023-07-16 10:19:15 +0900

    create certfile, keyfile, & server.py ignore certfile & keyfile

---
+++ server.py
@@ -0,0 +1,7 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='localhost.pem',
+                               keyfile='localhost-key.pem', server_side=True)
+httpd.serve_forever()
